8d67c88ce41bf1d72bc85a425bab101754f3ce52
Sarthak <sarthakss898@gmail.com>
2025-09-23 21:54:10 +0530

    Basic routes

--- fastapi-crash-course/main.py
+++ fastapi-crash-course/main.py
@@ -4,10 +4,40 @@ from typing import List
 
 app = FastAPI()
 
-class Tea(BaseModel):
+class Coffee(BaseModel):
     id : int
     name : str
     origin: str
 
-teas: List[Tea] = []
+coffees: List[Coffee] = []
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to coffee house"}
+
+@app.get("/coffees")
+def get_coffees():
+    return coffees
+
+@app.post("/coffees")
+def add_coffee(coffee: Coffee):
+    coffees.append(coffee)
+    return coffees
+
+@app.put("/coffees/{coffee_id}")
+def update_coffee(coffee_id: int, updated_coffee: Coffee):
+    for i,c in enumerate(coffees):
+        if c.id == coffee_id:
+            coffees[i] = updated_coffee
+            return updated_coffee
+    return {"error": "Coffee not found"}
+
+@app.delete("/coffees/{coffee_id}")
+def delete_coffee(coffee_id):
+    newcoffees:List[Coffee] = []
+    for i,c in enumerate(coffees):
+        if coffees[i] == coffee_id:
+            continue
+        else:
+            newcoffees.append(c)
+    return coffees
