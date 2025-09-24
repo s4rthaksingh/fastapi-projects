029420d2e6a055de0e55137801ec6d81d825a23c
Sarthak <sarthakss898@gmail.com>
2025-09-24 20:11:51 +0530

    Say URL message

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/{msg}")
+async def say(msg):
+    return {"message": msg}
