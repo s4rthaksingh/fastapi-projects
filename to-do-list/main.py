816cba0a69a1053e96ee3585aec024f8e10b2a87
Sarthak <sarthakss898@gmail.com>
2025-09-24 20:24:47 +0530

    Delete todo

--- to-do-list/main.py
+++ to-do-list/main.py
@@ -12,3 +12,12 @@ async def root():
 async def create_todo(todo):
     todos.append(todo)
     return todos
+
+@app.get("/delete/{todo_id}")
+async def delete_todo(todo_id: int):
+    if todo_id  < len(todos):
+        return {"Todo deleted" : todos.pop(todo_id),
+                "To-Do List" : todos}
+    else:
+        return {"Error": "No todo found with that ID",
+                "To-Do List" : todos}
